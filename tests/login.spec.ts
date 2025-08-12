23dc7982c81bb0fd2575f1369b4c0328401d4c7a
Yevheniia Zhuravel <baranenko87@gmail.com>
2025-08-12 21:40:05 +0200

    extracted user info into variables

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,17 +1,21 @@
 import { test, expect } from '@playwright/test';
 
+const user_email = 'customer@practicesoftwaretesting.com';
+const user_password = 'welcome01';
+const user_name = 'Jane Doe';
+
 test('login', async ({ page }) => {
   test.skip(!!process.env.CI, 'Test is skipped in CI due to the Cloudflare protection.');
   
   await page.goto('/auth/login');
   
-  await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
-  await page.getByTestId('password').fill('welcome01');
+  await page.getByTestId('email').fill(user_email);
+  await page.getByTestId('password').fill(user_password);
   await page.getByRole('button', { name: 'Login' }).click();
  
   await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
   await expect(page.getByTestId('page-title')).toContainText('My account');
-  await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
+  await expect(page.getByTestId('nav-menu')).toContainText(user_name);
 
 
 
